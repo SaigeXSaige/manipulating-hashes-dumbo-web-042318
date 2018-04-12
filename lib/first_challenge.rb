af8e39b11f65f4ec5011f728a4e5ffabaf7fceef
Saige Leslie <saigeleslie@gmail.com>
2018-04-12 06:06:03 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,6 +15,8 @@ def first_challenge
 
   #your code here
   contacts.each do |person, data|
+    data.each do |attribute, value|
+      
 
   #remember to return your newly altered contacts hash!
   contacts
