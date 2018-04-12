48f64ad5524118663b957487604772093badddca
Saige Leslie <saigeleslie@gmail.com>
2018-04-12 06:18:20 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
           value.delete_if { 
-              |item| item == "strawberry"
+              |flavor| flavor == "strawberry"
             }
         end
       end
